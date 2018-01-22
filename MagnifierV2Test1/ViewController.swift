d3f57c081578fdefec11c30c911112970553595e
Nicholas Rosato <nicholasrosato@Nicholass-MacBook-Pro.local>
2018-01-22 13:46:01 -0500

    First commit

---
+++ MagnifierV2Test1/ViewController.swift
@@ -0,0 +1,53 @@
+//
+//  ViewController.swift
+//  MagnifierV2Test1
+//
+//  Created by Nicholas Rosato on 1/14/18.
+//  Copyright © 2018 EPICSMagnifierTeam. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBAction func controlButtons(_ sender: UIButton) {
+       // buttonPressed(senderTag: sender.tag)
+    }
+    
+ /*   func buttonPressed(senderTag : Int){
+        if senderTag == 0
+        {
+            print ("Device Images")
+            print (senderTag)
+        }
+        else if senderTag == 1
+        {
+            print ("Fullscreen")
+            print (senderTag)
+        }
+        else if senderTag == 2
+        {
+            print ("Flip Image")
+            print (senderTag)
+        }
+        else if senderTag == 3
+        {
+            print ("Snapshot")
+            print (senderTag)
+        }
+    }*/
+    
+    
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
