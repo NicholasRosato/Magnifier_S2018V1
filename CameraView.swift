5a7eea62b9219f1aa3c8608cddfcfd6358b06984
Nicholas Rosato <nicholasrosato@pal-nat186-110-102.itap.purdue.edu>
2018-01-29 14:39:08 -0500

    Prototype UI1

---
+++ CameraView.swift
@@ -0,0 +1,35 @@
+//
+//  CameraView.swift
+//  MagnifierV2Test1
+//
+//  Created by Nicholas Rosato on 1/28/18.
+//  Copyright © 2018 EPICSMagnifierTeam. All rights reserved.
+//
+
+import UIKit
+
+class CameraView: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
